d8f685843a7615581aeb8006966faca04870a000
Juan <juancarrasquer@gmail.com>
2025-02-14 18:54:29 +0100

    primeras migraciones

---
+++ app/Models/Producto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'productos';
+
+    protected $fillable = ['nombre', 'precio', 'proveedor_id', 'descuento_id', 'descripcion', 'cantidad'];
+
+    public function proveedor()
+    {
+        return $this->belongsTo(Proveedor::class, 'proveedor_id');
+    }
+
+    public function descuento()
+    {
+        return $this->belongsTo(Descuento::class, 'descuento_id');
+    }
+}
